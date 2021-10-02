a936964a2bcc5742fb26a464bcd35f639fc2a27b
Tehmi Queen <tehreem2019@namal.edu.pk>
2021-10-02 08:51:36 -0700

    Div Functionality

---
+++ cal04.py
@@ -0,0 +1,11 @@
+#Calculator in Python(v0.4)
+def sub(num1,num2):
+  return num1-num2
+def mul(num1,num2):
+  return num1 * num2
+def add(num1,num2):
+  return num1 + num2
+def div(num1,num2):
+  return num1 + num2
+#Main
+print(div(6,2),mul(3,2),sub(4,2),add(7,1))
