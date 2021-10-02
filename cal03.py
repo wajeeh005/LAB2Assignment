898a13f94368cab0f69894b0a752d46ba3273cbf
Hurmat Ilyas <hurmat2019@namal.edu.pk>
2021-10-02 20:37:48 +0500

    Mul Functionality

---
+++ cal03.py
@@ -0,0 +1,9 @@
+#Calculator in Python(v0.2)
+def sub(num1,num2):
+  return num1-num2
+def mul(num1,num2):
+  return num1 * num2
+def add(num1,num2):
+  return num1 + num2
+#Main
+print(mul(3,2),sub(4,2),add(7,1))
